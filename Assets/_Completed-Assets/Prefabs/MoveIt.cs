32a35317fca28b1ec004ecffa0a6dc8878023cba
Jason Scourge <jasonscourge@hotmail.com>
2017-10-19 17:00:15 +0800

    All files uploaded and updated

---
+++ Assets/_Completed-Assets/Prefabs/MoveIt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveIt : MonoBehaviour {
+	public float speed = 25f;
+
+	private Vector2 dir; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Moves from top to down
+		transform.Translate (dir * Time.deltaTime);
+	}
+
+	public void setDirection(Vector2 chosenDir){
+		dir = chosenDir;
+	}
+}
