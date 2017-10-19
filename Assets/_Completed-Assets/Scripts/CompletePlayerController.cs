514ad8a6e4ce1a8505f5b6affa42f62e2efb8deb
Jason Scourge <jasonscourge@hotmail.com>
2017-10-19 10:44:20 +0800

    Reupdate of files with gitignore

--- Assets/_Completed-Assets/Scripts/CompletePlayerController.cs
+++ Assets/_Completed-Assets/Scripts/CompletePlayerController.cs
@@ -8,10 +8,12 @@ public class CompletePlayerController : MonoBehaviour {
 
 	public float speed;				//Floating point variable to store the player's movement speed.
 	public Text countText;			//Store a reference to the UI Text component which will display the number of pickups collected.
-	public Text winText;			//Store a reference to the UI Text component which will display the 'You win' message.
+	public Text loseText;			//Store a reference to the UI Text component which will display the 'You win' message.
 
 	private Rigidbody2D rb2d;		//Store a reference to the Rigidbody2D component required to use 2D Physics.
 	private int count;				//Integer to store the number of pickups collected so far.
+	private bool hasLost; 			//To determine if the game is over yet.
+	private float updateRespawn;	//Respawn timer for the rocks.
 
 	// Use this for initialization
 	void Start()
@@ -20,10 +22,13 @@ public class CompletePlayerController : MonoBehaviour {
 		rb2d = GetComponent<Rigidbody2D> ();
 
 		//Initialize count to zero.
-		count = 0;
+		count = 1;
+
+		//Initialize win to false.
+		hasLost = false; 
 
 		//Initialze winText to a blank string since we haven't won yet at beginning.
-		winText.text = "";
+		loseText.text = " ";
 
 		//Call our SetCountText function which will update the text with the current value for count.
 		SetCountText ();
@@ -38,11 +43,22 @@ public class CompletePlayerController : MonoBehaviour {
 		//Store the current vertical input in the float moveVertical.
 		float moveVertical = Input.GetAxis ("Vertical");
 
+		//Store the current time in the float currentTime.
+		float currentTime = Time.deltaTime;
+
 		//Use the two store floats to create a new Vector2 variable movement.
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 
-		//Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-		rb2d.AddForce (movement * speed);
+		if (hasLost) {
+			// Preventing the object to move any further.
+			speed = 0;
+
+			// Removing all current velocity from the object.
+			rb2d.velocity = Vector2.zero; 
+		} else {
+			//Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+			rb2d.AddForce (movement * speed);
+		}
 	}
 
 	//OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
@@ -51,14 +67,23 @@ public class CompletePlayerController : MonoBehaviour {
 		//Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
 		if (other.gameObject.CompareTag ("PickUp")) 
 		{
-			//... then set the other object we just collided with to inactive.
-			other.gameObject.SetActive(false);
-			
+
 			//Add one to the current value of our count variable.
-			count = count + 1;
+			count = count - 1;
 			
 			//Update the currently displayed count by calling the SetCountText function.
 			SetCountText ();
+
+			if (count <= 0) {
+				hasLost = true; 
+			}
+				
+			if (hasLost) {
+				loseText.text = "You LOSE!";
+			}
+				
+			gameObject.SetActive(false);
+			Time.timeScale = 0;
 		}
 		
 
@@ -67,12 +92,6 @@ public class CompletePlayerController : MonoBehaviour {
 	//This function updates the text displaying the number of objects we've collected and displays our victory message if we've collected all of them.
 	void SetCountText()
 	{
-		//Set the text property of our our countText object to "Count: " followed by the number stored in our count variable.
-		countText.text = "Count: " + count.ToString ();
-
-		//Check if we've collected all 12 pickups. If we have...
-		if (count >= 12)
-			//... then set the text property of our winText object to "You win!"
-			winText.text = "You win!";
+		countText.text = "Life: " + count.ToString ();
 	}
 }
---
+++ Assets/_Completed-Assets/Scripts/Spawning.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour {
+	public GameObject sharkRock;
+	public GameObject[] allSpawnPoints;
+	public int start;
+	public int interval;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("spawnRocks", start, interval);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	void spawnRocks(){
+		int rand = Random.Range(0, allSpawnPoints.Length); 
+		Vector2 chosenSpawnPoint = allSpawnPoints[rand].transform.position;
+		GameObject item = Instantiate(sharkRock, chosenSpawnPoint, Quaternion.identity);
+	}
+}
