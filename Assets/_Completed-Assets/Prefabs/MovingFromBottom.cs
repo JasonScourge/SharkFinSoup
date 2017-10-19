bdb617854f291c9c52c6b5f7363926be3d669be3
Jason Scourge <jasonscourge@hotmail.com>
2017-10-19 14:27:14 +0800

    Reset Folder created to prevent issues with commiting

---
+++ Assets/_Completed-Assets/Prefabs/MovingFromBottom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingFromBottom: MonoBehaviour {
+
+	public float speed = 50f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector2(0, speed) * Time.deltaTime);
+	}
+}
