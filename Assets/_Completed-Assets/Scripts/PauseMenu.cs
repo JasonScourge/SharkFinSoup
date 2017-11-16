fd24f55d226aa39ba6b60018e53b2011a2b2f94e
jasonscourge <jasonscourge@hotmail.com>
2017-11-16 17:22:52 +0800

    Add on pause countdown when resuming game

--- Assets/_Completed-Assets/Scripts/PauseMenu.cs
+++ Assets/_Completed-Assets/Scripts/PauseMenu.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour {
-
 	public GameObject pauseMenu;
 	public GameObject pauseButton;
+	public GameObject[] countdown;
 
 	private float StoredTime; 
 	private bool isPaused = false;
@@ -20,6 +20,7 @@ public class PauseMenu : MonoBehaviour {
 		/// They mean very different things
 		/// Read the suggestions or documentations in detail before choosing which one to use
 		if (Input.GetKeyDown("p")) {
+			// Necessary to check if it is paused or not before deciding to pause or unpause
 			if (!isPaused) {
 				isPaused = true;
 				pauseGame ();
@@ -35,18 +36,46 @@ public class PauseMenu : MonoBehaviour {
 	}
 
 	public void pauseGame(){
-		// This one is to stop the time
+		// Store the current game time
 		StoredTime = Time.timeScale;
+
+		// This one is to stop the time
 		Time.timeScale = 0.00001f;
+
+		// Removes the pause button and creates pause screen
 		pauseMenu.SetActive (true);
 		pauseButton.SetActive (false);
+
+		// Update current state if it is paused or not
 		isPaused = true;
 	}
 
 	public void resumeGame(){
-		Time.timeScale = StoredTime;
+		// Recreates the pasue button and deactivates the pause screen
 		pauseMenu.SetActive (false);
 		pauseButton.SetActive (true);
+
+		// Triggering the countdown
+		StartCoroutine ("resumeTiming");
+	}
+
+	IEnumerator resumeTiming() {
+		// This loop handles the countdown timer display
+		for (int i = 0; i < countdown.Length; i++) {
+			if (i == 0) {
+				countdown [i].SetActive (true);
+			} else {
+				countdown [i].SetActive (true);
+				countdown [i - 1].SetActive (false);
+			}
+			yield return new WaitForSecondsRealtime (1.1f);
+		}
+		countdown [countdown.Length - 1].SetActive (false);
+
+		// Returning the game back to its last saved game speed
+		Time.timeScale = StoredTime;
+
+		// Updates current state
 		isPaused = false;
 	}
 }
