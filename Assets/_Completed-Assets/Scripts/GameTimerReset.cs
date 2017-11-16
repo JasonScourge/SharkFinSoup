c2885a16bb48dfa0f2d80a3ffc497e147721cfa7
jasonscourge <jasonscourge@hotmail.com>
2017-11-16 17:27:06 +0800

    Create a GameTimerReset.cs and implement it into the gameManager of losing and menu scene

--- Assets/_Completed-Assets/Scripts/GameTimerReset.cs
+++ Assets/_Completed-Assets/Scripts/GameTimerReset.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawning : MonoBehaviour {
+public class GameTimerReset: MonoBehaviour {
+	
+	void Start()
+	{
+		Time.timeScale = 1.0f;
+	}
 
+	void Update(){
+		Time.timeScale = 1.0f;
+	}
 }
